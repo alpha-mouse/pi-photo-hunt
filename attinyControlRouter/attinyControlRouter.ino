5680702ed77572bc1412b527b16ca294e0d3ad4e
Ivan Bakhtin <ivan.bachtin@gmail.com>
2020-06-21 19:00:13 +0300

    timeout for autopiloting as a safety measure

--- attinyControlRouter/attinyControlRouter.ino
+++ attinyControlRouter/attinyControlRouter.ino
@@ -4,7 +4,9 @@
 #define CLEAR(reg, b) (reg &= ~_BV(b))
 #define IS_SET(reg, b) (reg & _BV(b))
 
-#define RC_OVERRIDE_THRESHOLD 0.08
+#define RC_LOW_THRESHOLD 0.076
+#define RC_HIGH_THRESHOLD 0.08
+#define AUTOPILOTING_DURATION_THRESHOLD ((unsigned long)5 * 60 * 1000)
 
 const int resolution = 20;
 float counts[resolution];
@@ -12,6 +14,10 @@ int currentCounter = 0;
 bool initiated = false;
 unsigned long pulseStart = 0;
 unsigned long pulseEnd = 0;
+
+int previousAutopilotRequest = 0;
+unsigned long autopilotStart = 0;
+bool onAutopilot = false;
 
 volatile bool rcOverride = false;
 
@@ -22,15 +28,17 @@ ISR(INT0_vect)
   if (initiated) {
     unsigned long m = micros();
     if (currentLevel) {
-      float pulseLength = pulseEnd - pulseStart;
-      if (600 < pulseLength && pulseLength < 2600) {
-        counts[currentCounter++ % resolution] = pulseLength / (m - pulseStart);
-        float sum = 0;
-        for (int i = 0; i < resolution; ++i) {
-          sum += counts[i];
+      if (m - pulseStart != 0) { // not sure, how it could have been 0, but just in case
+        float pulseLength = pulseEnd - pulseStart;
+        if (600 < pulseLength && pulseLength < 2600) {
+          counts[currentCounter++ % resolution] = pulseLength / (m - pulseStart);
+          float sum = 0;
+          for (int i = 0; i < resolution; ++i) {
+            sum += counts[i];
+          }
+          float currentMotorDutyCycle = sum / resolution;
+          rcOverride = RC_LOW_THRESHOLD < currentMotorDutyCycle && currentMotorDutyCycle < RC_HIGH_THRESHOLD;
         }
-        float currentMotorDutyCycle = sum / resolution;
-        rcOverride = currentMotorDutyCycle > RC_OVERRIDE_THRESHOLD;
       }
       pulseStart = m;
     } else {
@@ -65,13 +73,25 @@ void setup() {
 
 void loop() {
   bool switchToAutopilot;
-  if (rcOverride) {
+  if (rcOverride || (onAutopilot && (millis() - autopilotStart > AUTOPILOTING_DURATION_THRESHOLD))) {
     switchToAutopilot = false;
   } else {
     // if PB3 is high, which is if autopilot control is requested
-    switchToAutopilot = IS_SET(PINB, PINB3);
+    int currentAutopilotRequest = IS_SET(PINB, PINB3);
+    if (previousAutopilotRequest ^ currentAutopilotRequest) {
+      previousAutopilotRequest = currentAutopilotRequest;
+      switchToAutopilot = currentAutopilotRequest;
+    }
   }
   if (switchToAutopilot) {
+    if (!onAutopilot) {
+      onAutopilot = true;
+      autopilotStart = millis();
+    }
+  } else {
+    onAutopilot = false;
+  }
+  if (onAutopilot) {
     SET(PORTB, PB0);
   } else {
     CLEAR(PORTB, PB0);
